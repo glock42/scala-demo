ab07e1cc0b4aec4b271fc1115bd64985ccd0538b
threezj <threezj@hotmail.com>
2016-07-01 00:24:39 +0800

    finish hw4 part1-2

---
+++ patmat/src/main/scala/test.scala
@@ -0,0 +1,10 @@
+import patmat.Huffman
+
+/**
+  * Created by "Threezj" on 16-6-30.
+  */
+object test {
+    def main(args: Array[String]) {
+        println(Huffman.times(Huffman.string2Chars("aba")))
+    }
+}
