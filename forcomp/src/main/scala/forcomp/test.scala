40460e8e170072522dffb2a797061485738fb1a5
threezj <threezj@hotmail.com>
2016-07-15 16:23:07 +0800

    finish hw6

---
+++ forcomp/src/main/scala/forcomp/test.scala
@@ -0,0 +1,11 @@
+package forcomp
+
+import forcomp.Anagrams.{Occurrences, Word}
+
+/**
+  * Created by "Threezj" on 16-7-14.
+  */
+object test {
+    def main(args: Array[String]): Unit = {
+    }
+}
